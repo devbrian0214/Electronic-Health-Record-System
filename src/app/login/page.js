b37018f60bcbbcd51710c948f30d6ebf68a75b74
devbrian0214 <elitedevbrianh@gmail.com>
2023-10-07 15:44:37 +0500

    Login page created

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import Image from "next/image";
+import logo from "@app/assets/img/icons/login-icon-02.svg";
+import LoginForm from "@app/components/forms/LoginForm";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className='main-wrapper login-body'>
+      <div className='container-fluid px-0'>
+        <div className='row'>
+          <div className='col-lg-6 login-wrap'>
+            <div className='login-sec'>
+              <div className='log-img'>
+                {/* <Image src={logo} alt='' /> */}
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-6 login-wrap-bg'>
+            <div className='login-wrapper'>
+              <div className='loginbox'>
+                <div className='login-right'>
+                  <div className='login-right-wrap'>
+                    <h2>Sign In</h2>
+                    <LoginForm />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>
-        EHR-System
+        
       </h1>
     </main>
   )
