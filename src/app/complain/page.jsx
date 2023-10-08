7b5363aa811d3daf3e479b7c9a7abe792054dad7
devbrian0214 <elitedevbrianh@gmail.com>
2023-10-08 19:55:04 +0500

    Vital page created, Profile CSS needs to be fixed

--- src/app/complain/page.jsx
+++ src/app/complain/page.jsx
@@ -50,22 +50,20 @@ const page = () => {
                 </div>
                 </div>  
 
-
-
        <div className='card-box p-4'>
         <h3 className='card-title mb-3'>History of Presenting Complain</h3>
         <div className='row'>
           <div className='col-md-12'>
           
             <div className='profile-basic'>
-            <h5>1.Pain</h5>
+            <h5 className='myfield'>1. Pain</h5>
               <div className='row'>
               <div className='col-md-6'>
                   <div className='form-group local-forms'>
                     <label className='focus-label'>Character</label>
                     <select
                       className='form-control select'
-                      name='gender'
+                      name='character'
                       >
                       <option value=''>Select Character</option>
                       <option value='Sharp'>Sharp</option>
@@ -82,63 +80,218 @@ const page = () => {
                         </label>
                         <div className="form-check-inline">
                         <label className="form-check-label">
-                            <input type="radio" name="Unilateral Localized" className="form-check-input" />
+                            <input type="radio" name="Site" className="form-check-input" />
                             Unilateral Localized
                         </label>
                         </div>
                         <div className="form-check-inline">
                         <label className="form-check-label">
-                            <input type="radio" name="Bilateral Localized" className="form-check-input" />
+                            <input type="radio" name="Site" className="form-check-input" />
                             Bilateral Localized
                         </label>
-                    </div>
+                        </div>
 
-                    <div className="form-check-inline">
+                        <div className="form-check-inline">
                         <label className="form-check-label">
-                            <input type="radio" name="Unilateral Localized" className="form-check-input" />
+                            <input type="radio" name="Site" className="form-check-input" />
                             Generalized
                         </label>
                         </div>
-        </div>
+                     </div>
                 </div>
-             
-               
-              
-                
-                        
-               
-
+                <div className='col-md-6'>
+                  <div className='form-group local-forms'>
+                    <label className='focus-label'>Radiating Pain</label>
+                    <select
+                      className='form-control select'
+                      name='radiating pain'
+                      >
+                      <option value=''>Select Radiating Pain</option>
+                      <option value='Yes'>Yes</option>
+                      <option value='No'>No</option>
+                    
+                    </select>
+                  </div>
+                </div>
+                <div className='col-md-6'>
+                  <div className='form-group local-forms'>
+                    <label className='focus-label'>Timing</label>
+                    <select
+                      className='form-control select'
+                      name='timing'
+                      >
+                      <option value=''>Select Timing</option>
+                      <option value='constant'>Remains Constant</option>
+                      <option value='variable'>Changes throughout the day</option>
+                    
+                    </select>
+                  </div>
+                </div>
+                <div className='col-md-6'>
+                  <div className='form-group local-forms'>
+                    <label className='focus-label'>Exacerbating & Alleriating Factor</label>
+                    <select
+                      className='form-control select'
+                      name='ExacerbatingFactor'
+                      >
+                      <option value=''>Select Exacerbating & Alleriating Factor</option>
+                      <option value='exacerbate'>Exacerbating with Physical Abilities</option>
+                      <option value='alleviates'>Alleviates with rest</option>
+                    
+                    </select>
+                  </div>
+                </div>
               </div>
+
+            
+            <div className='row'>
+              <div className='col-md-6'>
+              <h5 className='col-md-6 myfield'>2. Onset of Sign & Symptoms</h5>
+                  <div className='form-group local-forms'>
+                    <label className='focus-label'>Sign & Symptoms</label>
+                    <select
+                      className='form-control select'
+                      name='symptoms' placeholder='Select Sign & Symptoms'
+                      >
+                      <option value=''>Select Sign & Symptoms</option>
+                      <option value='Acute'>Acute</option>
+                      <option value='Chronic'>Chronic</option>
+                     
+                    </select>
+                  </div>
+                </div>
+            </div>
+            
+            <div className=" col-md-6 ">
+            <h5 className='col-md-6 myfield'>3. Swelling</h5>
+                <div className="form-group local-forms">
+                <label>
+                Site <span className="login-danger">*</span>
+                </label>
+                <input className="form-control" type="text" />
+                </div>
+            </div>
+          
+            <h5 className='myfield'>4. Severity of Symptoms</h5>
+            <div className="col-12 col-md-6 col-xl-6">
+                <div className="form-group select-gender">
+                 <label className="gen-label">Severity of Symptoms
+                <span className="login-danger">*</span>
+                    </label>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    0
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    1
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    2
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    3
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    4
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    5
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    6
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    7
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    8
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    9
+                   </label>
+                   </div>
+                   <div className="form-check-inline">
+                    <label className="form-check-label">
+                   <input type="radio" name="Severity" className="form-check-input" />
+                    10
+                   </label>
+                   </div>
+                </div>
+            </div>
             </div>
           </div>
         </div>
       </div>
 
+{/* Part 5 Starts here */}
 
-
-              {/* <div className='card-box'>
-              <h3 className='card-title mb-3'>History of Presenting Complain</h3>
-              <div className='row'>
-              <div className=' col-md-12'>
-            <div className='form-group local-forms'>
-              <label className='focus-label'>Medical History</label>
+<div className='card-box p-4'>
+        <h3 className='card-title mb-3'>History of Presenting Complain</h3>
+        <div className='row'>
+        <div className='col-md-12'>
+        <div className='profile-basics'></div>
+        <div className='row'>
+              <div className="col-12 col-md-6 col-xl-4">
+                <div className="form-group local-forms">
+                  
+                <label className='focus-label'>Constitutional Symptoms</label>
+                    <select
+                      className='form-control select'
+                      name='symptoms' placeholder=''
+                      >
+                      <option value=''>Constitutional Symptoms</option>
+                      <option value='Acute'>Acute</option>
+                      <option value='Chronic'>Chronic</option>
+                     
+                    </select>
+                </div>
+              </div>
+         
+  
             
-               </div> */}
+      </div>
+        </div>
+        </div>
+      </div>
 
-          
-            {/* </div>
-            </div>
-            </div> */}
-
+           
           <div className='col-md-12'>
             <button
               type='submit'
               className='btn btn-primary btn-rounded center'
             >
-              
+              Submit
             </button>
           </div>
-       </div>
+    </div>
         </form>
         </div>
               </div>
