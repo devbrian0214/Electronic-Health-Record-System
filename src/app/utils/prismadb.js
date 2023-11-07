395fda7c43e7544aa19735cc08b6e25af889e29b
devbrian0214 <elitedevbrianh@gmail.com>
2023-11-07 19:00:39 +0500

    updates

--- src/app/utils/prismadb.js
+++ src/app/utils/prismadb.js
@@ -1,8 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const client =
-  globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+  globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export default client;
